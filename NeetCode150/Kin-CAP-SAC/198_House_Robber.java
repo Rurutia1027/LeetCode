b5e7e7b12ea771736824e5a4f893ce554a96689f
Rurutia1027 <rurutia1027@gmail.com>
2025-05-15 09:25:10 +0000

    add two pointer refine version for House Robber I

--- NeetCode150/Kin-CAP-SAC/198_House_Robber.java
+++ NeetCode150/Kin-CAP-SAC/198_House_Robber.java
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/house-robber/description/?envType=problem-list-v2&envId=plakya4j
 
-class Solution {
+class Solution1 {
     public int rob(int[] nums) {
         int n = nums.length; 
         if (n == 0) return 0; 
@@ -18,5 +18,27 @@ class Solution {
         }
 
         return dp[n-1]; 
+    }
+}
+
+// optimized version 
+class Solution2 {
+    public int rob(int[] nums) {
+        int n = nums.length; 
+        if (n == 0) return 0; 
+        if (n == 1) return nums[0]; 
+
+        // we use two pointer to replace dp array 
+        int prev1 = nums[0]; // dp[0]
+        int prev2 = Math.max(nums[0], nums[1]); // dp[1]
+
+        for (int i = 2; i < n; i++) {
+            // dp[i] = MAX(dp[i - 2] + nums[i], dp[i - 1])
+            int current = Math.max(prev1 + nums[i], prev2); 
+            prev1 = prev2; 
+            prev2 = current;         
+        }
+
+        return Math.max(prev1, prev2); 
     }
 }
